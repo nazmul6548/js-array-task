c25b3493daea19a8bc6c55163b0b1622a4e74a81
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-28 16:13:39 +0600

    task2

---
+++ task1.js
@@ -0,0 +1,13 @@
+/*
+1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
+// ans:
+
+const myArray = ["mango", "watermelon", "grape", "apple", "orange"];
+console.log(myArray[3]);
+myArray[2]= "jambura"
+console.log(myArray);
