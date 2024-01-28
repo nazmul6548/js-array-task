cece4a17c83b2c49f7706bdead492d104b332b48
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-28 21:55:06 +0600

    slice

---
+++ slice.js
@@ -0,0 +1,14 @@
+// extraction methods :
+// 1.slice
+// 2.substring
+// 3.substr
+// now we start slice
+const mySlice = ["apple","orange","banana","cat","cow"];
+// const result = mySlice.slice(2);
+// console.log(result);
+
+// const output = mySlice.slice(1,-3);
+// console.log(output);
+// const textMes = "good for nothing";
+// const res = textMes.slice(2,-8);
+// console.log(res);
