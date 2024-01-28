d62c950691a46fc2a8b8ec790b896aba6162510b
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-28 17:07:03 +0600

    task5

---
+++ task5.js
@@ -0,0 +1,11 @@
+// 5. Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+// ans:
+const arrays = [1,2,3];
+const arrays2 = [9,5,8];
+const result =arrays.concat(arrays2);
+console.log(result);
