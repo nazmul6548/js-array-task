2757b78290db351c3503bc719e491be3c989cbce
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-28 23:32:37 +0600

    slice

--- slice.js
+++ slice.js
@@ -11,7 +11,12 @@ const mySlice = ["apple","orange","banana","cat","cow"];
 // console.log(output);
 // const textMes = "good for nothing";
 // const res = textMes.slice(2,-8);
+
 // console.log(res);
+// console.log(ttt);
+// var myLetter = 'a,b,c,d,e,f,g,h';
+// var result = myLetter.split();
+// console.log(result);
 
 
 // const mySlicee = ["apple","orange","banana","cat","cow"];
@@ -21,12 +26,32 @@ const mySlice = ["apple","orange","banana","cat","cow"];
 // console.log(mySlicee);
 // mySlicee.pop();
 // console.log(mySlicee);
+// mySlicee.split(".")
 
-const myNumber = [1,2,3,4,5,6,7,8];
-const includess =myNumber.includes(4);
-console.log(includess);
-const index =myNumber.indexOf(4);
-console.log(index);
-const another =myNumber.splice(1,0 ,800);
-console.log(another);
+// const myNumber = [1,2,3,4,5,6,7,8];
+// const includess =myNumber.includes(4);
+// console.log(includess);
+// const index =myNumber.indexOf(4);
+// console.log(index);
+// const another =myNumber.splice(1,0 ,800);
+// console.log(another);
+// console.log(myNumber);
+
+
+
+
+// var myNumber =0
+// var yourNumber =1
+// var result =  myNumber / yourNumber ;
+// console.log(result);
+// var myNumber =0
+// var yourNumber =1
+// var result =yourNumber /  myNumber;
+// console.log(result);
+
+
+
+
+var myArray = ["apple","banana","orange","tomato"]
+const myNumber =myArray[myArray.length-1]
 console.log(myNumber);
