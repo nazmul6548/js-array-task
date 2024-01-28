ace5bedaf473427077e55c678cbcae4eead73874
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-28 16:55:24 +0600

    task4

---
+++ task4.js
@@ -0,0 +1,17 @@
+// 4. Checking if it's an Array
+// Instructions:
+
+// Create different variables, each containing either an array or a non-array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+// ans:
+const isText = "jewel"
+const num = 43;
+const isArrayValue = "[]";
+const isNum = [1, 2, 3, 4];
+console.log(Array.isArray(isArrayValue));
+console.log(Array.isArray(isText));
+console.log(Array.isArray(isNum));
+console.log(Array.isArray(isText));
