f7163c992057e1b80cf333a23da9df5dae98313c
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-29 18:24:18 +0600

    push pop

--- variable.js
+++ variable.js
@@ -1,5 +1,22 @@
 console.log(options);
-var options = "nazmul";
+var options = 767;
 options = "islam"
 options = "jewel"
-console.log(options);
+// console.log(options);
+var number =[1,2,333  ,4,5]
+// console.log(number.length);
+var num =number.indexOf(333)
+number[2]= 7777
+// console.log(number);
+
+
+
+var myNumber= [1,3, 4,5,4,5,45,];
+myNumber[1] = 999
+// console.log(myNumber);
+
+var anyNumber = [1,2,3,4,5,]
+anyNumber.pop();
+anyNumber.pop();
+anyNumber.push(123)
+console.log(anyNumber);
