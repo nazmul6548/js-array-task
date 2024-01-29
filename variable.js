f36096fdc8a1408cdd6f10a22e9171ece912cdfc
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-29 19:16:00 +0600

    parsenint

--- variable.js
+++ variable.js
@@ -5,18 +5,36 @@ options = "jewel"
 // console.log(options);
 var number =[1,2,333  ,4,5]
 // console.log(number.length);
-var num =number.indexOf(333)
-number[2]= 7777
+// var num =number.indexOf(333)
+// number[2]= 7777
 // console.log(number);
 
 
 
-var myNumber= [1,3, 4,5,4,5,45,];
-myNumber[1] = 999
+// var myNumber= [1,3, 4,5,4,5,45,];
+// myNumber[1] = 999
 // console.log(myNumber);
 
 var anyNumber = [1,2,3,4,5,]
 anyNumber.pop();
 anyNumber.pop();
 anyNumber.push(123)
-console.log(anyNumber);
+// console.log(anyNumber);
+
+
+// 
+var mame = "alam"
+var newe = mame;
+// console.log(newe);
+mame = "motu";
+// console.log(mame);
+
+
+
+// 
+let myNumber2= "87";
+myNumber2=parseInt(myNumber2)
+let myNumber1 = 55;
+console.log(typeof myNumber2);
+let eresult= myNumber2+myNumber1
+console.log(eresult);
