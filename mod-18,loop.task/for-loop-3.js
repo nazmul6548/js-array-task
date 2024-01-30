1e885620739133d4d0b38bedba364bfd64b2a88b
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-30 12:09:06 +0600

    task

--- mod-18,loop.task/for-loop-3.js
+++ mod-18,loop.task/for-loop-3.js
@@ -3,13 +3,9 @@
 Generate a multiplication table for number 9
 
  */
-for (let index = 0; index <= 10; index++) {
-    if (index * 9 !== 0) {
-        multiple = index * 9
+for (let index = 1; index <= 10; index++) {
+    multiple = index * 9
         console.log(index,"* 9 =",multiple);
-        
-    }
-    
     
 }
 // console.log(multiple);
