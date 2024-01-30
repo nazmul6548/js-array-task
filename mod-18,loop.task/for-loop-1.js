523f40df1ae929701be8e91d0193be53b58e9e9d
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-30 10:43:41 +0600

    even odd

--- mod-18,loop.task/for-loop-1.js
+++ mod-18,loop.task/for-loop-1.js
@@ -5,7 +5,11 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
-
+for (let index = 61; index < 100; index++) {
+  if (index % 2 !== 0) {
+    console.log(index, "odd number");
+  }
+}
 
 /***
 
@@ -14,5 +18,10 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
+for (let i = 78; i < 98; i++) {
+  if (i % 2 === 0) {
+    console.log(i, "even number");
+  }
+}
 
 /*programming hero*/
