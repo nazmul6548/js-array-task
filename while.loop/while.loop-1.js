b4dd965721600681b9970cfb119ca4f857dceae2
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-30 12:15:15 +0600

    while

--- while.loop/while.loop-1.js
+++ while.loop/while.loop-1.js
@@ -5,6 +5,18 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
+let num = 61;
+while (num < 100) {
+    if (num % 2 !==0) {
+        
+        console.log(num,"odd number");
+        
+        
+        
+    }
+    num++
+    
+}
 
 
 /***
@@ -14,5 +26,14 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
+let myNum = 78;
+while (myNum < 98)  {
+    if (myNum % 2 === 0) {
+        console.log(myNum,"even numbers");
+        
+    }
+    myNum++
+    
+}
 
 /*programming hero*/
