46c33935e697bd87659a20de022cce9cf164c887
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-30 12:35:12 +0600

    while

---
+++ while.loop/whilwloop-3.js
@@ -0,0 +1,13 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+let count = 1;
+while (count <= 10) {
+    console.log(count,"* 5 =",count * 5);
+    count++;
+}
+
+
+/*programming hero*/
