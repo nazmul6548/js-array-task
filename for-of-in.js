284d831f956648226153b6a7cd6a8b089bc11a0b
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-30 10:32:40 +0600

    for loop

---
+++ for-of-in.js
@@ -0,0 +1,10 @@
+// let number = [1, 2, 3, 4, 5, 6, 7];
+// for (let jewel of number) {
+//     console.log(jewel);
+    
+// };
+let text = "programming";
+for (const result of text) {
+    console.log(result);
+    
+}
--- task5.js
+++ task5.js
@@ -8,4 +8,16 @@
 const arrays = [1,2,3];
 const arrays2 = [9,5,8];
 const result =arrays.concat(arrays2);
-console.log(result);
+// console.log(result);
+let num = 0;
+let sum=0
+while (num <= 10) {
+    console.log(num  );
+    // num = num +1
+    // num +=1
+    sum=sum+num
+    console.log("sum:",sum);
+    num++;
+
+    
+}
