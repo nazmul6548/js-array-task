e1c773a1c3d8e62fcb35816f2fe5d9d4f214e488
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-30 15:51:28 +0600

    continue

--- continue-loop/continue-loop.js
+++ continue-loop/continue-loop.js
@@ -1,4 +1,23 @@
 /*
 display odd number from 55 to 85 and skip the numbers divisible by 5.
 */
-for
+// for example
+for (let i = 55; i <= 85; i++) {
+    if (i % 2 === 0) {
+        continue;
+        
+    }
+    console.log(i);
+    
+}
+
+// while examples
+let num = 55;
+while (num < 85) {
+    num++;
+    if (num % 2 === 0) {
+        continue;
+        
+    }
+    console.log(num);
+}
